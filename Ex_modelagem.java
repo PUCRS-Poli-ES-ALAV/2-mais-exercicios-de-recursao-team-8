02cefdcc890ac70d9e1a56656d24d33d2adcc66e
gustavoburgie <gustavoburgie@gmail.com>
2025-08-06 19:38:53 -0300

    Continuação aula passada

---
+++ Ex_modelagem.java
@@ -0,0 +1,49 @@
+public class Ex_modelagem {
+    public static void main(String args[]){
+        System.out.println(fatorial(3));
+        System.out.println(somatorio(0));
+        System.out.println(fibon(6));
+        System.out.println(double_somatorio(1,6));
+    }   
+    public static int fatorial(int n){
+        if(n == 0)
+        return 1;
+        else if(n < 0)
+        return -9999;
+        else
+        return n * fatorial(n-1);
+    }
+
+    public static int somatorio(int n){
+        if(n > 0)
+        return somatorio(n-1);
+        if(n < 0)
+        return somatorio(n+1);
+        else
+        return 0;
+    }
+
+    public static int fibon(int n){
+        if(n <= 0)
+        return 0;
+        if(n == 1)
+        return 1;
+        else
+        return fibon(n-1) + fibon(n-2);
+    }
+
+    public static int double_somatorio(int k, int j){
+        if(k<j)
+        return k + double_somatorio(k+1, k);
+        if(k>j)
+        return double_somatorio(j, k);
+        else
+        return 0;
+    }
+
+    public static boolean pal(String s){
+        
+        return false;
+    }
+}
+
